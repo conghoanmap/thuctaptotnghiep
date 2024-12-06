ed51fd71c2196ee6ac8808ecdb4165966f33844a
chien15122003 <chienvan1203@gmail.com>
2024-12-06 21:56:48 +0700

    thêm tailwindcss

--- inc/dev.php
+++ inc/dev.php
@@ -1,1 +1,6 @@
 <?php
+function vanchien_add_tailwindcss()
+{
+	wp_enqueue_style('tailwind-style', get_template_directory_uri() . '/assets/css/output.css', array(), '1.0.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'vanchien_add_tailwindcss');
